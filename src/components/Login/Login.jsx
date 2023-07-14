59d31517976b89158358be94167b19a60385eb54
IconicLuck <uhasib959@gmail.com>
2023-07-14 20:19:56 +0600

    implemented firebase login and creating user

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,44 @@
+import app from '../../firebase/firebase.config';
+import './Login.css'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth(app)
 
 const Login = () => {
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+
+        console.log(email, password)
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            alert('User has been logged in successfully')
+        })
+        .catch(error => {
+            alert(`${error.message}`)
+        })
+    }
+
     return (
-        <div>
-            <h1 className="text-4xl">This is Login Component</h1>
+        <div className='login-container'>
+            <form onSubmit={handleLogin}>
+                <div className="flex-col mt-5">
+                    <label htmlFor="email">Email</label>
+                    <input className='input-field' type="email" name="email" id="email" placeholder='Your Email' />
+                </div>
+                <div className='flex-col mt-5'>
+                    <label htmlFor="password">Password</label>
+                    <input className='input-field' type="password" name="password" id="password" placeholder='Your Password' />
+                </div>
+                <div className='text-center'>
+                    <input className='login-btn' type="submit" value="Login" />
+                </div>
+            </form>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,44 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+
+const auth = getAuth(app)
 
 
 const Register = () => {
+
+    const handleRegister = (event) => {
+        event.preventDefault()
+
+
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            alert('User has been created successfully')
+        })
+        .catch(error => {
+            alert(`${error.message}`)
+        })
+    }
+
     return (
-        <div>
-            <h1>This is Register component</h1>
+        <div className='login-container'>
+            <form onSubmit={handleRegister}>
+                <div className="flex-col mt-5">
+                    <label htmlFor="email">Email</label>
+                    <input className='input-field' type="email" name="email" id="email" placeholder='Your Email' required />
+                </div>
+                <div className='flex-col mt-5'>
+                    <label htmlFor="password">Password</label>
+                    <input className='input-field' type="password" name="password" id="password" placeholder='Your Password' required />
+                </div>
+                <div className='text-center'>
+                    <input className='login-btn' type="submit" value="Register" />
+                </div>
+            </form>
         </div>
     );
 };
