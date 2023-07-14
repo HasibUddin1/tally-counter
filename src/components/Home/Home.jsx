2cd5aa20dca7501b571144654ad50f77b2923766
IconicLuck <uhasib959@gmail.com>
2023-07-14 20:04:10 +0600

    implemented counter functionality

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,50 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './Home.css'
+import { decrementCounter, incrementCounter, resetCounter, setCounter } from "../../reduxServices/actions/counterActions";
+import { useRef } from "react";
 
 
 const Home = () => {
 
+    const valueRef = useRef()
+
     const count = useSelector(state => state.count)
+
+    const dispatch = useDispatch()
+
+    const handleIncrement = () => {
+        dispatch(incrementCounter())
+    }
+
+    const handleDecrement = () => {
+        dispatch(decrementCounter())
+    }
+
+    const handleReset = () => {
+        dispatch(resetCounter())
+    }
+
+    const handleStartValue = () => {
+        dispatch(setCounter(parseFloat(valueRef.current.value)))
+    }
 
     return (
         <div className="count-container">
-            <h1 className="count">Count: {count}</h1>
+            <div className="tally-counter-container">
+                <h1>Tally Counter</h1>
+                <h2 className="count">Count: {count}</h2>
+                <div>
+                    <button onClick={handleIncrement} className="button-add">Add</button>
+                    <button onClick={handleDecrement} className="button-reduce">Reduce</button>
+                    <button onClick={handleReset} className="button-reset">Reset</button>
+                    <div className="start-value-container">
+                        <input ref={valueRef} className="value-input" type="number" name="" id="" placeholder="Start Value" />
+                        <div className="text-center">
+                            <button onClick={handleStartValue} className="button-start-value">Enter Your Start Value</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
